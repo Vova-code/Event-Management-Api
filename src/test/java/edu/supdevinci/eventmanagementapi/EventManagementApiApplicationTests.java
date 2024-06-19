f6a80fdced930f12eca77c6d20845b4c7abd0f9e
Vova-code <vdoche@live.fr>
2024-06-19 14:09:28 +0200

    Init project: Spring Initializr

---
+++ src/test/java/edu/supdevinci/eventmanagementapi/EventManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.supdevinci.eventmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
