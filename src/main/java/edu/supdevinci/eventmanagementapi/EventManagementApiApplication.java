879266f8d3c83edb03bec4fa03d8c48bf44e70e2
Massiré Touré <73407799+Massire9@users.noreply.github.com>
2024-06-22 19:07:17 +0200

    Update EventManagementApiApplication.java

--- src/main/java/edu/supdevinci/eventmanagementapi/EventManagementApiApplication.java
+++ src/main/java/edu/supdevinci/eventmanagementapi/EventManagementApiApplication.java
@@ -3,7 +3,9 @@ package edu.supdevinci.eventmanagementapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 @EnableJpaRepositories
 public class EventManagementApiApplication {
